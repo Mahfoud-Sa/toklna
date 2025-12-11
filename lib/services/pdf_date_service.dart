a37d877cf64a9854e499d77a4bf5c7e611cca8f0
Mahfoud <binsabbah2013@gmail.com>
2025-12-11 09:07:17 +0300

    change the data and time loaction in the file

--- lib/services/pdf_date_service.dart
+++ lib/services/pdf_date_service.dart
@@ -21,8 +21,7 @@ class PdfDateService {
     // 3. Get the LAST page
     final PdfPage page = document.pages[document.pages.count - 1];
 
-    // 4. Get page size to calculate bottom position
-    final double pageWidth = page.getClientSize().width;
+    // 4. Get page height to calculate vertical center position
     final double pageHeight = page.getClientSize().height;
 
     // 5. Get today's date formatted
@@ -30,12 +29,17 @@ class PdfDateService {
       'yyyy/MM/dd - HH:mm',
     ).format(DateTime.now());
 
-    // 6. Add date text at bottom-left of the last page
-    // Position: 20 from left, 30 from bottom
+    // 6. Add date text aligned to the left, near the bottom
+    const double textWidth = 200;
+    const double textHeight = 20;
+    const double leftMargin = 20;
+    const double bottomMargin = 175; // Distance from bottom
+    final double yPosition = pageHeight - bottomMargin;
+
     page.graphics.drawString(
       todayDate,
       PdfStandardFont(PdfFontFamily.helvetica, 12),
-      bounds: Rect.fromLTWH(20, pageHeight - 30, 200, 20),
+      bounds: Rect.fromLTWH(leftMargin, yPosition, textWidth, textHeight),
       brush: PdfSolidBrush(PdfColor(0, 0, 0)), // Black color
     );
 
